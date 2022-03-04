059cc58ae6b4ee0efcde4d6b693066a6d77c0207
tyagiashish74 <tyagiashish74@gmail.com>
2022-03-05 01:16:04 +0530

    Updates

---
+++ precache-manifest.5f5aa0ee1aa3e6692d8bfcfad78b86a2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b52e282ced076f606dd",
+    "url": "/stablecoin/static/css/main.4a11ac5a.chunk.css"
+  },
+  {
+    "revision": "0b52e282ced076f606dd",
+    "url": "/stablecoin/static/js/main.0b52e282.chunk.js"
+  },
+  {
+    "revision": "6b1c8b995fa1b9ef9d5e",
+    "url": "/stablecoin/static/css/1.627a5188.chunk.css"
+  },
+  {
+    "revision": "6b1c8b995fa1b9ef9d5e",
+    "url": "/stablecoin/static/js/1.6b1c8b99.chunk.js"
+  },
+  {
+    "revision": "e2f50e671a462d69f4e6",
+    "url": "/stablecoin/static/js/runtime~main.e2f50e67.js"
+  },
+  {
+    "revision": "ef0faf2e181dfac5584ccee977cf16b8",
+    "url": "/stablecoin/static/media/logo.ef0faf2e.webp"
+  },
+  {
+    "revision": "f945e17b5465496bb775be06a3a02ea4",
+    "url": "/stablecoin/static/media/banner1.f945e17b.webp"
+  },
+  {
+    "revision": "07c858841c62f05070d0dbf26a2b1710",
+    "url": "/stablecoin/static/media/banner2.07c85884.webp"
+  },
+  {
+    "revision": "f743eb5e398836ad3895184a64fac37d",
+    "url": "/stablecoin/static/media/banner3.f743eb5e.png"
+  },
+  {
+    "revision": "48ef1477c204b574f1186b7e83c0928c",
+    "url": "/stablecoin/static/media/banner4.48ef1477.webp"
+  },
+  {
+    "revision": "1ff78010139baae56d4822b7434f2253",
+    "url": "/stablecoin/static/media/banner5.1ff78010.webp"
+  },
+  {
+    "revision": "723fab4a9061a2d3585566feb2408e81",
+    "url": "/stablecoin/static/media/1.723fab4a.webp"
+  },
+  {
+    "revision": "6260c9e13de8e1691d2ba89930c3e287",
+    "url": "/stablecoin/static/media/2.6260c9e1.webp"
+  },
+  {
+    "revision": "fd48550dbf1db3d8eafc8aaff0f0b03c",
+    "url": "/stablecoin/static/media/3.fd48550d.webp"
+  },
+  {
+    "revision": "2904c31aaa9aef8b7f14a19e9fa7d9d3",
+    "url": "/stablecoin/static/media/4.2904c31a.webp"
+  },
+  {
+    "revision": "e9edc4fbe48232097df5676b0dc8c926",
+    "url": "/stablecoin/static/media/zerofee.e9edc4fb.webp"
+  },
+  {
+    "revision": "5d5705097077d26881f5b8f11785a076",
+    "url": "/stablecoin/index.html"
+  }
+];
